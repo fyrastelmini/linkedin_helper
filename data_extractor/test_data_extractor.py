a0e0486409a67336becaa73d20cbdc97c3a60c42
Fyras Telmini <69505085+fyrastelmini@users.noreply.github.com>
2024-06-02 19:01:45 +0000

    fixed data-extractor workflow

--- data_extractor/test_data_extractor.py
+++ data_extractor/test_data_extractor.py
@@ -2,7 +2,72 @@ import pytest
 from unittest.mock import patch, MagicMock
 import os
 from database import Job,db,ma
-from data_extractor import extract_div_content, create_consumer, consume_messages
+def extract_div_content(url, div_class):
+
+    try:
+        response = requests.get(url, timeout=5)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            target_div = soup.find("div", {"class": div_class})
+
+            if target_div:
+                job_title = (
+                    target_div.find("h1", class_="top-card-layout__title")
+                    .text.strip()
+                    .split("Login & Sign Up")[0]
+                )
+                company_name = target_div.find(
+                    "a", class_="topcard__org-name-link"
+                ).text.strip()
+                location = target_div.find(
+                    "span", class_="topcard__flavor--bullet"
+                ).text.strip()
+                return jsonify({
+                    "job_title": job_title,
+                    "company_name": company_name,
+                    "location": location,
+                    "URL": url,
+                })
+            else:
+                return jsonify({"error": f"Div with class '{div_class}' not found on the page."}), 404
+
+        else:
+            return jsonify({"error": f"Failed to retrieve the page. Status code: {response.status_code}"}), 500
+    except:
+        return jsonify({"error": "An error occurred while processing the request."}), 500
+
+def create_consumer():
+    while True:
+        try:
+            consumer = KafkaConsumer('new_raw_data',
+                                     bootstrap_servers='kafka:9092',
+                                     auto_offset_reset='earliest',
+                                     enable_auto_commit=True,
+                                     group_id='my-group',
+                                     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+            return consumer
+        except NoBrokersAvailable:
+            print("Broker not available, retrying...")
+            time.sleep(3)
+
+
+def consume_messages():
+    consumer = create_consumer()
+
+    for message in consumer:
+        url = message.value['url']
+        div_class = message.value['div_class']
+        with app.test_request_context():
+            result = extract_div_content(url, div_class)
+        if result:  # Check the status code
+            text = result.get_json()
+
+            if text:
+                new_job = Job(text["job_title"], text["company_name"], text["location"], text["URL"])
+                with app.app_context():
+                    db.session.add(new_job)
+                    db.session.commit()
 os.environ['DATABASE_URL'] = 'sqlite:////tmp/test.db'
 from flask import Flask
 app = Flask(__name__)
